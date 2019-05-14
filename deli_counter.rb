2e0d670f8b878aa886b0c16d052eb4faad199083
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-14 15:06:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,15 @@ def line(katz_deli)
     def take_a_number (katz_deli,name)
       if katz_deli.length==0 
       katz_deli.push(name)
-      puts "Welcome {#name}. You are number #{i+1} in line. 
+      puts "Welcome {#name}. You are number #{katz_deli.length} in line. 
+    
+    elsif katz_deli.length>=1 
+       katz_deli.push(name)
+      puts "Welcome {#name}. You are number #{katz_deli.length} in line.
+    
+  else 
+     katz_deli.push(name)
+    
     end 
     
 def now_serving(katz_deli)
