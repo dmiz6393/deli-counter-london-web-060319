813b4b32a0eecbc0854ee049510cd217e002e82b
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-14 15:32:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,3 +12,29 @@ def line(katz_deli)
     end
   end 
  
+    def take_a_number (katz_deli,name)
+      if katz_deli.length==0 
+      katz_deli.push(name)
+      puts "Welcome {#name}. You are number #{katz_deli.length} in line."  
+    
+    elsif katz_deli.length>=1 
+       katz_deli.push(name)
+      puts "Welcome {#name}. You are number #{katz_deli.length} in line." 
+  
+  else 
+     katz_deli.push(name)
+    "Welcome {#name}. You are number #{katz_deli.length} in line."
+    end 
+  end 
+  
+def now_serving(katz_deli)
+if katz_deli.length==0 
+  puts "There is nobody waiting to be served!"
+else 
+puts "Currently serving #{katz_deli.first}."
+end 
+katz_deli.shift 
+end 
+end 
+end 
+end 
