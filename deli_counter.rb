92172f01bbf7d3cabfb10e8f6eebf0760e919bc0
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-14 15:01:09 +0000

    Automatically backed up by Learn

---
+++ deli_counter.rb
@@ -0,0 +1,20 @@
+katz_deli = [] 
+
+def line(katz_deli)
+  if katz_deli.length>0 
+  katz_deli.each_with_index |name,index| 
+   puts "The line is currently: {#index+1}. {#name}"
+  else "The line is currently empty."
+    end
+  end 
+    
+    def take_a_number (katz_deli,name)
+      puts "Welcome {#name}. You are number #{i+1} in line. 
+    end 
+    
+def now_serving(katz_deli)
+puts "Currently serving 
+.pop 
+  
+else 
+puts "There is nobody waiting to be served!"
