bf7e82f0aab85324dfe44537aa38b5f2c46fd6b1
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-14 15:22:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,13 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
 
- else katz_deli.each_with_index do |name,index| 
-   puts "The line is currently:" + "{#index+1}.{#name}"
+ else 
+   line= "The line is currently:"
+   katz_deli.each_with_index do |name,index| 
+   line.concat ("#{index+1}.#{name}") 
+   puts line 
     end
   end 
- end 
  
     def take_a_number (katz_deli,name)
       if katz_deli.length==0 
