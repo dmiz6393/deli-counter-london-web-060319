ffccf05d9a5fe11a56edf65f177043c7a9bce1f1
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-14 15:15:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length>0 
   katz_deli.each_with_index do |name,index| 
-   puts "The line is currently: {#index+1}. {#name}"
-  else "The line is currently empty."
+   puts "The line is currently:" + "{#index+1}. {#name}"
+  else puts "The line is currently empty."
     end
   end 
 end 
