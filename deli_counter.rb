db198c711c19e1aaaf9d18859320a44983e330dd
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-14 15:16:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 katz_deli = [] 
 
 def line(katz_deli)
-  if katz_deli.length>0 
-  katz_deli.each_with_index do |name,index| 
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+ else katz_deli.each_with_index do |name,index| 
    puts "The line is currently:" + "{#index+1}. {#name}"
-  else puts "The line is currently empty."
     end
   end 
 end 
